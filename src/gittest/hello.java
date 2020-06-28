3703b79a448b6bc0488e603188b83aa2af7c4e1f
bianlunhui <bianlunhye@163.com>
2020-06-28 15:41:14 +0800

    1

---
+++ src/gittest/hello.java
@@ -0,0 +1,13 @@
+package gittest;
+
+public class hello {
+
+	public static void main(String[] args) {
+	System.out.println("tijiao ");
+	System.out.println("tijiao1 ");
+	System.out.println("tijiao 2");
+	System.out.println("tijiao3 ");
+
+	}
+
+}
